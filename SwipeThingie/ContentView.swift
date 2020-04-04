e759ce08f31c25dbcf12be46f439369deafe92db
Eric Lewis <ericlewis777@gmail.com>
2020-04-04 11:24:48 -0400

    cell

--- SwipeThingie/ContentView.swift
+++ SwipeThingie/ContentView.swift
@@ -7,10 +7,40 @@
 //
 
 import SwiftUI
+import MapKit
+
+struct MapSnapshot: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView()
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        
+    }
+}
 
 struct ContentView: View {
+    
+    var roundedRect: RoundedRectangle {
+        RoundedRectangle(cornerRadius: 10)
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading, spacing: 0) {
+            HStack {
+                Image(systemName: "gear")
+                VStack(alignment: .leading) {
+                    Text("Title")
+                    Text("Subbbbbb")
+                }
+            }
+            .padding()
+            MapSnapshot()
+        }
+        .aspectRatio(contentMode: .fit)
+        .mask(roundedRect)
+        .overlay(roundedRect.stroke(Color.secondary, lineWidth: 5))
+        .padding()
     }
 }
 
