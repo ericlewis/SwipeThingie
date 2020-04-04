9f4337e1a5d6b74b76766a3a4e9eba33e8501387
Eric Lewis <ericlewis777@gmail.com>
2020-04-04 11:13:10 -0400

    Initial Commit

---
+++ SwipeThingie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwipeThingie
+//
+//  Created by Eric Lewis on 4/4/20.
+//  Copyright © 2020 Eric Lewis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
